08d6f5fd7b26667421ef300798e3442109206234
Brian Beltran <brianbeltran93@gmail.com>
2023-04-08 17:11:38 -0400

    FaceRecognition box
    -Added ability to add a box to picture provided at given coordinates of a face

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const setupClarifaiRequestOptions = (imageUrl) => {
   const USER_ID = 'brianb93';       
   const APP_ID = 'my-first-application';
   // Change these to whatever model and image URL you want to use
-  const MODEL_ID = 'face-detection';
+  //const MODEL_ID = 'face-detection';
   const IMAGE_URL = imageUrl;
 
   const raw = JSON.stringify({
@@ -61,6 +61,24 @@ class App extends Component {
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
@@ -68,10 +86,12 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
 
-    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", setupClarifaiRequestOptions(this.state.input))
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", setupClarifaiRequestOptions(this.state.input))
     .then(response => response.json())
     .then(response => {
         console.log('hi', response.outputs[0].data.regions[0].region_info.bounding_box)
+        this.displayFaceBox(this.calculateFaceLocation(response))
+        .catch(err => console.log(err));
         // if (response) {
         //     fetch('http://localhost:3000/image', {
         //         method: 'put',
@@ -96,7 +116,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
